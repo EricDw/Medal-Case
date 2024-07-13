e4e008f2bb9ca256bd872f9f28b0166586198315
EricDw <dewildte@gmail.com>
2024-07-13 19:01:12 -0400

    Update resources a few modifications

--- app/src/main/java/com/challenge/medalcase/ui/theme/Type.kt
+++ app/src/main/java/com/challenge/medalcase/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.challenge.medalcase.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -16,6 +17,7 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Bold,
         fontSize = 14.sp,
+        color = MediumGray,
     ),
     labelMedium = TextStyle(
         fontFamily = FontFamily.Default,
@@ -24,7 +26,8 @@ val Typography = Typography(
     ),
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Bold,
         fontSize = 12.sp,
+        color = LightGray,
     ),
 )
